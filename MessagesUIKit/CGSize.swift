8440aa8c5b926d3950ccd3e5edeb26484be8ce7e
Kevin Wooten <kevin@wooten.com>
2016-05-25 08:21:26 -0600

    CGSize extensions (swift) are now private

--- MessagesUIKit/CGSize.swift
+++ MessagesUIKit/CGSize.swift
@@ -11,28 +11,28 @@ import Foundation
 
 extension CGSize {
   
-  public var halfHeight : CGFloat {
+  var halfHeight : CGFloat {
     return height/2
   }
   
-  public var halfWidth : CGFloat {
+  var halfWidth : CGFloat {
     return width/2
   }
   
 }
 
-public func -(lhs: CGSize, rhs: CGSize) -> CGSize {
+func -(lhs: CGSize, rhs: CGSize) -> CGSize {
   return CGSize(width: lhs.width - rhs.width, height: lhs.height - rhs.height)
 }
 
-public func +(lhs: CGSize, rhs: CGSize) -> CGSize {
+func +(lhs: CGSize, rhs: CGSize) -> CGSize {
   return CGSize(width: lhs.width + rhs.width, height: lhs.height + rhs.height)
 }
 
-public func +=(inout lhs: CGSize, rhs: CGSize) {
+func +=(inout lhs: CGSize, rhs: CGSize) {
   lhs = lhs+rhs
 }
 
-public func -=(inout lhs: CGSize, rhs: CGSize) {
+func -=(inout lhs: CGSize, rhs: CGSize) {
   lhs = lhs-rhs
 }
